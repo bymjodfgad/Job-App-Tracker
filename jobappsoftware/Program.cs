720c079c6fbff900aa04b6f899d1386d4f582c0a
bymjodfgad <bymjodfgad@gmail.com>
2025-10-12 19:04:41 +0200

    Add project files.

---
+++ jobappsoftware/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jobappsoftware
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
