0c7a53ac8ab71ca3ff6ef4670506c91f2f42de16
bymjodfgad <bymjodfgad@gmail.com>
2025-10-14 17:54:40 +0200

    First Commit... Added JobManager, Menu & Application classes.

---
+++ jobappsoftware/JobManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace jobappsoftware
+{
+    public class JobManager
+    {
+        public List<JobApplication> Applications { get; private set; }
+
+        public JobManager()
+        {
+            Applications = new List<JobApplication>();
+        }
+
+        public void AddJob()
+        {
+            Console.Write("Företagsnamn: ");
+            string company = Console.ReadLine();
+
+            Console.Write("Jobbtitel: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Löneanspråk (kr): ");
+            int salary = int.Parse(Console.ReadLine());
+
+            var job = new JobApplication(company, title, salary);
+            Applications.Add(job);
+
+            Console.WriteLine("Ansökan tillagd!\n");
+        }
+
+        public void ShowAll()
+        {
+            if (Applications.Count == 0)
+            {
+                Console.WriteLine("Inga ansökningar att visa.\n");
+                return;
+            }
+
+            Console.WriteLine("Alla ansökningar:");
+            foreach (var app in Applications)
+            {
+                Console.WriteLine(app.GetSummary());
+            }
+            Console.WriteLine();
+        }
+
+        public void UpdateStatus()
+        {
+            ShowAll();
+            Console.Write("Ange index på ansökan att uppdatera (börjar från 0): ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index >= 0 && index < Applications.Count)
+            {
+                Console.WriteLine("Välj ny status:");
+                foreach (var status in Enum.GetValues(typeof(ApplicationStatus)))
+                    Console.WriteLine($"{(int)status} - {status}");
+
+                Console.Write("Ditt val: ");
+                int choice = int.Parse(Console.ReadLine());
+                Applications[index].Status = (ApplicationStatus)choice;
+
+                Console.WriteLine("Status uppdaterad!\n");
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt val.\n");
+            }
+        }
+
+        public void RemoveJob()
+        {
+            ShowAll();
+            Console.Write("Ange index på ansökan att ta bort: ");
+            if (int.TryParse(Console.ReadLine(), out int index) && index >= 0 && index < Applications.Count)
+            {
+                Applications.RemoveAt(index);
+                Console.WriteLine("Ansökan borttagen!\n");
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt val.\n");
+            }
+        }
+    }
+}
---
+++ jobappsoftware/Menu.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jobappsoftware
+{
+    public class Menu
+    {
+        private readonly JobManager _manager;
+        private bool _running = true;
+
+        public Menu(JobManager manager)
+        {
+            _manager = manager;
+        }
+
+        public void Run()
+        {
+            while (_running)
+            {
+                ShowMenu();
+                HandleInput();
+            }
+        }
+
+        private void ShowMenu()
+        {
+            Console.WriteLine("========== Job Application Tracker ==========");
+            Console.WriteLine("1. Lägg till ny ansökan");
+            Console.WriteLine("2. Visa alla ansökningar");
+            Console.WriteLine("3. Uppdatera status");
+            Console.WriteLine("4. Ta bort en ansökan");
+            Console.WriteLine("0. Avsluta");
+            Console.Write("Välj ett alternativ: ");
+        }
+
+        private void HandleInput()
+        {
+            string choice = Console.ReadLine();
+            Console.Clear();
+
+            switch (choice)
+            {
+                case "1":
+                    _manager.AddJob();
+                    break;
+                case "2":
+                    _manager.ShowAll();
+                    break;
+                case "3":
+                    _manager.UpdateStatus();
+                    break;
+                case "4":
+                    _manager.RemoveJob();
+                    break;
+                case "0":
+                    _running = false;
+                    Console.WriteLine("Programmet avslutas...");
+                    break;
+                default:
+                    Console.WriteLine("Ogiltigt val.\n");
+                    break;
+            }
+        }
+    }
+}
